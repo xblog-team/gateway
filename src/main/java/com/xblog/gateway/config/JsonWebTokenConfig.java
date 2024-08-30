c2b0587b5fd731e853b5f90576f6f0f5df9adb8c
jki12 <sjak0227@naver.com>
2024-08-30 14:34:50 +0900

    Feat: jwt config 추가

---
+++ src/main/java/com/xblog/gateway/config/JsonWebTokenConfig.java
@@ -0,0 +1,26 @@
+package com.xblog.gateway.config;
+
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class JsonWebTokenConfig {
+    @Value("${custom.jwt.secret}")
+    private String secret;
+
+    @Bean
+    public JwtParser jwtParser() {
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.parser()
+                .verifyWith(key)
+                .build();
+    }
+}
